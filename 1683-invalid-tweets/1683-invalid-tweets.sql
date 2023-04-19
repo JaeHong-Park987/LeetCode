2b421adfd61be0bfa0985fecdae809cf88444507
JaeHong-Park987 <jar987@naver.com>
2023-04-19 23:37:34 +0900

    Update 1683-invalid-tweets.sql

--- 1683-invalid-tweets/1683-invalid-tweets.sql
+++ 1683-invalid-tweets/1683-invalid-tweets.sql
@@ -2,3 +2,7 @@
 SELECT tweet_id
 FROM Tweets
 WHERE LENGTH(content) > 15 # 문자열 길이 : LENGTH함수 활용!!
+
+* LENGTH VS CHAR_LENGTH
+LENGTH : 바이트 단위로 측정된 문자열의 길이를 반환 -> 부호가 포함되어 있을 때 원하는 값을 가져오지 못할 수 있다.
+CHAR_LENGTH : 문자로 측정된 문자열의 길이를 반환
