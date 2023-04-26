531b8669a11b63217eaf5e84f56c9a55359392b1
JaeHong-Park987 <jar987@naver.com>
2023-04-26 14:53:38 +0900

    Time: 966 ms (51.37%), Space: 0B (100.00%) - LeetHub

--- 1393-capital-gain-loss/1393-capital-gain-loss.sql
+++ 1393-capital-gain-loss/1393-capital-gain-loss.sql
@@ -1,12 +1,22 @@
 # Write your MySQL query statement below
+# SELECT
+#     S.stock_name,
+#     SUM(S.price) AS capital_gain_loss
+# FROM(SELECT
+#         stock_name,
+#         CASE
+#             WHEN operation = 'Buy' THEN price * -1
+#             ELSE price
+#         END AS price
+#     FROM Stocks) AS S
+# GROUP BY S.stock_name;
+
+# FROM에 서브쿼리를 쓰는 대신 SELECT문 SUM에 바로 CASE WHEN 구문을 넣어도 된다.
 SELECT
-    S.stock_name,
-    SUM(S.price) AS capital_gain_loss
-FROM(SELECT
-        stock_name,
-        CASE
+    stock_name,
+    SUM(CASE
             WHEN operation = 'Buy' THEN price * -1
             ELSE price
-        END AS price
-    FROM Stocks) AS S
-GROUP BY S.stock_name
+        END) AS capital_gain_loss
+FROM Stocks
+GROUP BY stock_name;
