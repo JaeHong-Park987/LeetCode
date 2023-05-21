3a8b7ffa314447138eee80cc0f42fe8df09c8206
JaeHong-Park987 <jar987@naver.com>
2023-05-21 19:51:17 +0900

    Time: 440 ms (93.49%), Space: 0B (100.00%) - LeetHub

--- 0570-managers-with-at-least-5-direct-reports/0570-managers-with-at-least-5-direct-reports.sql
+++ 0570-managers-with-at-least-5-direct-reports/0570-managers-with-at-least-5-direct-reports.sql
@@ -1,8 +1,9 @@
 # Write your MySQL query statement below
 SELECT name
-FROM Employee
-WHERE id in (SELECT
-                managerId
-             FROM Employee
-             GROUP BY managerId
-             HAVING COUNT(managerId) >= 5) # managerId의 값이 null인 경우가 5개 이상일 수도 있기 때문에 * 대신 컬럼명을 명시해주었다.
+FROM Employee E1
+JOIN (SELECT
+        managerId
+      FROM Employee
+      GROUP BY managerId
+      HAVING COUNT(managerId) >= 5) E2
+ON E1.id = E2.managerId
