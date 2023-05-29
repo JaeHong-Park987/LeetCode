525b18ca479a950035adfc790e7ab2aa77a6a6fa
JaeHong-Park987 <jar987@naver.com>
2023-05-30 07:12:05 +0900

    Time: 1214 ms (24.46%), Space: 0B (100.00%) - LeetHub

--- 1789-primary-department-for-each-employee/1789-primary-department-for-each-employee.sql
+++ 1789-primary-department-for-each-employee/1789-primary-department-for-each-employee.sql
@@ -3,19 +3,13 @@ SELECT
     employee_id,
     department_id
 FROM Employee
-WHERE (employee_id, department_id) 
-IN(
-    SELECT
-        employee_id,
-        department_id
-    FROM Employee
-    WHERE primary_flag = 'Y'
-)
-OR employee_id
-IN(
-    SELECT
-        employee_id
-    FROM Employee
-    GROUP BY employee_id
-    HAVING COUNT(employee_id) = 1
-)
+WHERE primary_flag = 'Y'
+
+UNION
+
+SELECT
+    employee_id,
+    department_id
+FROM Employee
+GROUP BY employee_id
+HAVING COUNT(employee_id) = 1
