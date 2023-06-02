50e0bc99078fe74ea29a1b0422255b9a2d67e510
JaeHong-Park987 <jar987@naver.com>
2023-06-02 19:32:31 +0900

    Time: 567 ms (8.82%), Space: 0B (100.00%) - LeetHub

--- 1661-average-time-of-process-per-machine/1661-average-time-of-process-per-machine.sql
+++ 1661-average-time-of-process-per-machine/1661-average-time-of-process-per-machine.sql
@@ -1,24 +1,11 @@
 # Write your MySQL query statement below
 SELECT
-  A1.machine_id,
-  ROUND(SUM(end_time - start_time) / COUNT(*),3) AS processing_time
-FROM(
-  SELECT
-    machine_id,
-    process_id,
-    timestamp AS end_time
-  FROM Activity
-  WHERE activity_type = 'end'
-  GROUP BY machine_id, process_id
-) A1
-JOIN(
-  SELECT
-    machine_id,
-    process_id,
-    timestamp AS start_time
-  FROM Activity
-  WHERE activity_type = 'start'
-  GROUP BY machine_id, process_id
-) A2
-ON A1.machine_id = A2.machine_id AND A1.process_id = A2.process_id
-GROUP BY A1.machine_id
+  a1.machine_id,
+  ROUND(SUM(a2.timestamp - a1.timestamp) / COUNT(*), 3) AS processing_time
+FROM Activity AS a1
+JOIN Activity AS a2
+ON a1.machine_id = a2.machine_id
+AND a1.process_id = a2.process_id
+AND a1.activity_type = 'start'
+AND a2.activity_type = 'end'
+GROUP BY a1.machine_id
