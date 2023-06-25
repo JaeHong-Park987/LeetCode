0156c8782f2d718384797f5279c0165c169b9654
JaeHong-Park987 <jar987@naver.com>
2023-06-25 10:10:36 +0900

    Time: 1894 ms (10.00%), Space: 0B (100.00%) - LeetHub

--- 1934-confirmation-rate/1934-confirmation-rate.sql
+++ 1934-confirmation-rate/1934-confirmation-rate.sql
@@ -1,29 +1,8 @@
 # Write your MySQL query statement below
 SELECT
-  user_id,
-  ROUND(IFNULL(confirm_count / (timeout_count + confirm_count),0),2) AS confirmation_rate
-FROM(
-SELECT
-  S.user_id,
-  IFNULL(timeout_count,0) AS timeout_count,
-  IFNULL(confirm_count,0) AS confirm_count
+    S.user_id,
+    ROUND(AVG(IF(action = 'confirmed', 1.00, 0.00)),2) AS confirmation_rate
 FROM Signups AS S
-LEFT JOIN(
-  SELECT
-  user_id,
-  COUNT(user_id) AS timeout_count
-FROM Confirmations
-WHERE action = 'timeout'
-GROUP BY user_id
-) AS T
-ON S.user_id = T.user_id
-LEFT JOIN (
-  SELECT
-  user_id,
-  COUNT(user_id) AS confirm_count
-FROM Confirmations
-WHERE action = 'confirmed'
-GROUP BY user_id
-) AS C
+LEFT JOIN Confirmations AS C
 ON S.user_id = C.user_id
-) AS a
+GROUP BY S.user_id
